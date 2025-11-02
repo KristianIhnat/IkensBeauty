24557fdf8da96fcfd3345164e688160009348b90
KristianIhnat <84373529+KristianIhnat@users.noreply.github.com>
2025-11-02 19:48:48 +0100

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -44,12 +44,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 onClick={scrollToHome}
                 className="cursor-pointer hover:opacity-80 transition-opacity duration-200"
               >
-                <img 
-                  src="images/logo.svg" 
-                  alt="Ikens Beauty" 
-                  className="h-36 md:h-48 w-auto"
-                  style={{ maxWidth: '420px' }}
-                />
+               <img 
+  src="images/logo.svg" 
+  alt="Ikens Beauty" 
+  className="h-28 w-auto"   // väčšia výška
+  style={{ maxWidth: '420px' }} // väčšia maximálna šírka
+/>
               </a>
             </div>
             
